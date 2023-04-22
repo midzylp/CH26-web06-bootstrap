22ebd67bc0e627d270ddbdb17dbc7fae42635a21
Luisin <luis.enrique.sanper@gmail.com>
2023-04-22 13:22:33 -0600

    Se añadio archivo de tu nombre y se linkeo al principal

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,48 @@ function changeColorToRed(){
 const refName= document.getElementById("name");
 refName.style.color = "red";
 }
+
+function changeColorToBlue(){
+    const refName= document.getElementById("name");
+    refName.style.color = "lightblue";
+    }
+
+function changeColorToYellow(){
+        const refName= document.getElementById("name");
+        refName.style.color = "yellow";
+        }
+
+function changeColorParagraph(color, refObj){
+console.log(refObj);
+refObj.style.color= color;
+}
+
+function resetColors(){
+    
+    const refObjs = document.getElementsByClassName("text-color");
+    console.log(refObjs);
+    for (let index = 0; index < refObjs.length; index++) {
+        const element = refObjs[index];
+        console.log(element);
+        element.style.color = "purple";
+        
+    }
+}
+
+function changeGreeting(){
+    //obtener un nombre del cliente
+    const clientName = getNameClient();
+    //TODO obtener referencia de label h1
+    const greetingReference = getReferenceOfIdGreetings();
+    //TODO cambiar el saludo
+    greetingReference.innerHTML = "Hola " + clientName;
+}
+
+function getNameClient(){
+    const clientName = prompt("Escribe tu nombre");
+    return clientName;
+}
+
+function getReferenceOfIdGreetings(){
+    return document.getElementById("greetings");
+}
